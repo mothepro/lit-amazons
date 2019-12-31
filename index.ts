40d0f0b84acb02eb1551261c149d8e869d3ed6a7
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2019-12-30 21:28:25 -0600

    Add the base engine element

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-// Remember to append ".js" for relative imports
+import { customElement, LitElement, property, html } from 'lit-element'
+import Engine, { Board, Color } from '@mothepro/amazons-engine'
+
+@customElement('lit-amazons')
+export default class extends LitElement {
+  private engine!: Engine
+
+  @property({ type: Array })
+  private board?: Board
+
+  connectedCallback() {
+    this.engine = new Engine(this.board)
+    this.engine.turnStarted.on(detail => this.dispatchEvent(new CustomEvent('turn-started', { detail })))
+    this.engine.pieceMoved.on(detail => this.dispatchEvent(new CustomEvent('piece-moved', { detail })))
+    this.engine.boardChanged.on(() => this.dispatchEvent(new CustomEvent('board-changed')))
+    this.engine.turnStarted.activate(Color.BLACK)
+  }
+
+  protected readonly render = () => html`
+  enginge
+    ${this.engine}
+    <pre>${JSON.stringify(this.board)}</pre>
+    ${JSON.stringify(this.engine)}
+  `
+}
